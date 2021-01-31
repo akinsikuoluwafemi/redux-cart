b3821b290115a559d9bd15e2719a7d462224f97c
Oluwafemi <akinsiku13@gmail.com>
2021-01-31 11:57:12 +0100

    create reducers for redux cart

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
+// redux stuff
+import { createStore } from 'redux';
+import reducer from './reducer';
+
+// intial store
+const initialStore = {
+  cart: cartItems,
+  total: 0,
+  amount: 0
+}
+
+
+
+const store = createStore(reducer, initialStore)
+
+
+
+
+
+function App() {
+  // cart setup
+
+  return (
+    <main>
+      <Navbar cart={store.getState()} />
+      <CartContainer cart={cartItems} />
+    </main>
+  );
+}
+
+export default App;
